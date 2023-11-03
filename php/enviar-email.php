269b20663b313b22b943391768622392a4b70cbe
idmr023 <67611158+idmr023@users.noreply.github.com>
2023-11-03 09:17:52 -0500

    Actualización en enviar-email.php

--- php/enviar-email.php
+++ php/enviar-email.php
@@ -24,8 +24,8 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.office365.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'idmr_02@outlook.com';                     //SMTP username
-    $mail->Password   = 'mividaesmiaA';                               //SMTP password
+    $mail->Username   = '';                     //SMTP username
+    $mail->Password   = '';                               //SMTP password
     $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
     $mail->Port       = 587;                //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
     $mail->CharSet = 'UTF-8';                                 
