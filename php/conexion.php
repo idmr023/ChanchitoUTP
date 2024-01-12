44f7bc230acba295a17bac22e9845451bfcabfe5
idmr023 <67611158+idmr023@users.noreply.github.com>
2024-01-12 11:08:19 -0500

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,4 +1,4 @@
 <?php 
-    $conexion=new mysqli("localhost","root","","banco","3306");
+    $conexion=new mysqli("localhost","id21783174_bdchanchito","cha$$4towAww","id21783174_dbbanco","3306");
     $conexion->set_charset("utf8");
 ?>
